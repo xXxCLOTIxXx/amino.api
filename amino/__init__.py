b6a3e2d8bedea17b20cee832935296fced7fb0d4
Xsarz <88231084+xXxCLOTIxXx@users.noreply.github.com>
2023-04-19 20:25:47 +0300

    First commit

--- amino/__init__.py
+++ amino/__init__.py
@@ -24,7 +24,7 @@ __author__ = 'Xsarz'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2023 Xsarz'
 __version__ = '1.0'
-__newest__ = '1.0'#loads(get("https://pypi.org/pypi//json").text)["info"]["version"]
+__newest__ = loads(get("https://pypi.org/project/amino.api/json").text)["info"]["version"]
 
 
 
